839f033a225aa21a3eb0b23bade9f5a4a6e769ef
HWTjac0 <37227880+HWTjac0@users.noreply.github.com>
2025-04-21 19:13:36 +0000

    Deploying to gh-pages from @ HWTjac0/JAQ@3a748bdbf82d864dd7314482741d5df5eb2c927b 🚀

--- search/all_18.js
+++ search/all_18.js
@@ -21,7 +21,7 @@ var searchData=
   ['yoruba_18',['Yoruba',['https://doc.qt.io/qt-6/qlocale.html#Language-enum',1,'QLocale']]],
   ['younggrass_19',['YoungGrass',['https://doc.qt.io/qt-6/qgradient.html#Preset-enum',1,'QGradient']]],
   ['youngpassion_20',['YoungPassion',['https://doc.qt.io/qt-6/qgradient.html#Preset-enum',1,'QGradient']]],
-  ['ytilt_21',['ytilt',['https://doc.qt.io/qt-6/qtabletevent.html#yTilt',1,'QTabletEvent::yTilt()'],['https://doc.qt.io/qt-6/qinputdevice.html#Capability-enum',1,'QInputDevice::YTilt']]],
+  ['ytilt_21',['ytilt',['https://doc.qt.io/qt-6/qinputdevice.html#Capability-enum',1,'QInputDevice::YTilt'],['https://doc.qt.io/qt-6/qtabletevent.html#yTilt',1,'QTabletEvent::yTilt()']]],
   ['yuv_22',['YUV',['https://doc.qt.io/qt-6/qpixelformat.html#ColorModel-enum',1,'QPixelFormat']]],
   ['yuv411_23',['YUV411',['https://doc.qt.io/qt-6/qpixelformat.html#YUVLayout-enum',1,'QPixelFormat']]],
   ['yuv420p_24',['YUV420P',['https://doc.qt.io/qt-6/qpixelformat.html#YUVLayout-enum',1,'QPixelFormat']]],
--- search/classes_e.js
+++ search/classes_e.js
@@ -2,15 +2,16 @@ var searchData=
 [
   ['scaledpixmapargument_0',['ScaledPixmapArgument',['https://doc.qt.io/qt-6/qiconengine-scaledpixmapargument.html',1,'QIconEngine']]],
   ['selection_1',['Selection',['https://doc.qt.io/qt-6/qabstracttextdocumentlayout-selection.html',1,'QAbstractTextDocumentLayout']]],
-  ['sentinel_2',['sentinel',['https://doc.qt.io/qt-6/qdirlisting-sentinel.html',1,'QDirListing']]],
-  ['separatetocombinedimagesamplermapping_3',['SeparateToCombinedImageSamplerMapping',['https://doc.qt.io/qt-6/qshader-separatetocombinedimagesamplermapping.html',1,'QShader']]],
-  ['state_4',['State',['https://doc.qt.io/qt-6/qaccessible-state.html',1,'QAccessible']]],
-  ['station_5',['Station',['../classStation.html',1,'']]],
-  ['stationhandler_6',['StationHandler',['../classStationHandler.html',1,'']]],
-  ['stationmodel_7',['StationModel',['../classStationModel.html',1,'']]],
-  ['stencilopstate_8',['StencilOpState',['https://doc.qt.io/qt-6/qrhigraphicspipeline-stencilopstate.html',1,'QRhiGraphicsPipeline']]],
-  ['storageblock_9',['StorageBlock',['https://doc.qt.io/qt-6/qshaderdescription-storageblock.html',1,'QShaderDescription']]],
-  ['stringresult_10',['StringResult',['https://doc.qt.io/qt-6/qcborstreamreader-stringresult.html',1,'QCborStreamReader']]],
-  ['strong_5fordering_11',['strong_ordering',['https://doc.qt.io/qt-6/qt-strong-ordering.html',1,'Qt']]],
-  ['systemid_12',['SystemId',['https://doc.qt.io/qt-6/qcalendar-systemid.html',1,'QCalendar']]]
+  ['sensor_2',['Sensor',['../classSensor.html',1,'']]],
+  ['sentinel_3',['sentinel',['https://doc.qt.io/qt-6/qdirlisting-sentinel.html',1,'QDirListing']]],
+  ['separatetocombinedimagesamplermapping_4',['SeparateToCombinedImageSamplerMapping',['https://doc.qt.io/qt-6/qshader-separatetocombinedimagesamplermapping.html',1,'QShader']]],
+  ['state_5',['State',['https://doc.qt.io/qt-6/qaccessible-state.html',1,'QAccessible']]],
+  ['station_6',['Station',['../classStation.html',1,'']]],
+  ['stationhandler_7',['StationHandler',['../classStationHandler.html',1,'']]],
+  ['stationmodel_8',['StationModel',['../classStationModel.html',1,'']]],
+  ['stencilopstate_9',['StencilOpState',['https://doc.qt.io/qt-6/qrhigraphicspipeline-stencilopstate.html',1,'QRhiGraphicsPipeline']]],
+  ['storageblock_10',['StorageBlock',['https://doc.qt.io/qt-6/qshaderdescription-storageblock.html',1,'QShaderDescription']]],
+  ['stringresult_11',['StringResult',['https://doc.qt.io/qt-6/qcborstreamreader-stringresult.html',1,'QCborStreamReader']]],
+  ['strong_5fordering_12',['strong_ordering',['https://doc.qt.io/qt-6/qt-strong-ordering.html',1,'Qt']]],
+  ['systemid_13',['SystemId',['https://doc.qt.io/qt-6/qcalendar-systemid.html',1,'QCalendar']]]
 ];
