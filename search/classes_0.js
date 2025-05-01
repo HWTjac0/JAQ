80abb6a5a3c773c66a2cab6b20c5fec2db583aca
HWTjac0 <37227880+HWTjac0@users.noreply.github.com>
2025-05-01 17:30:18 +0000

    Deploying to gh-pages from @ HWTjac0/JAQ@377754387b6e518a5fb51f6a906dc77874ba3cf8 🚀

--- search/classes_0.js
+++ search/classes_0.js
@@ -2,6 +2,7 @@ var searchData=
 [
   ['apiclient_0',['ApiClient',['../classApiClient.html',1,'']]],
   ['app_1',['App',['../classApp.html',1,'']]],
-  ['attribute_2',['attribute',['https://doc.qt.io/qt-6/qinputmethodevent-attribute.html',1,'QInputMethodEvent::Attribute'],['https://doc.qt.io/qt-6/qsggeometry-attribute.html',1,'QSGGeometry::Attribute']]],
-  ['attributeset_3',['AttributeSet',['https://doc.qt.io/qt-6/qsggeometry-attributeset.html',1,'QSGGeometry']]]
+  ['appcontext_2',['AppContext',['../classAppContext.html',1,'']]],
+  ['attribute_3',['attribute',['https://doc.qt.io/qt-6/qinputmethodevent-attribute.html',1,'QInputMethodEvent::Attribute'],['https://doc.qt.io/qt-6/qsggeometry-attribute.html',1,'QSGGeometry::Attribute']]],
+  ['attributeset_4',['AttributeSet',['https://doc.qt.io/qt-6/qsggeometry-attributeset.html',1,'QSGGeometry']]]
 ];
