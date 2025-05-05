c4709655b2e94e03449f8748b25161dc88cc07e5
HWTjac0 <jakub3020.jmy@gmail.com>
2025-05-05 08:14:52 +0200

    added test and populated README

--- README.md
+++ README.md
@@ -1,11 +1,43 @@
 # Jacs Air Quality app
 This is app for checking air quality in cities in Poland. Data is provided by [Główny Inspektora Ochrony Środowiska (GIOŚ)](https://powietrze.gios.gov.pl/pjp/home) and fetched from their [API](https://api.gios.gov.pl/pjp-api/swagger-ui/) (Inconsistency in file naming comes from writing app in QtCreator which does not do any capitalization in filename and the coming to Clion which handles filenames as you would expect)
 ## Features
-TODO
+- Fetching available stations details
+- Fetching details of sensors for given station
+- Fetching current sensor data
+- Fetching historical sensor data
+- Displaying sensor data on chart with information about: maximum value, minimum value, average value and trend
+- Displaying location of stations on map for the selected city
+- Finding nearest station to the users current location
 ## Documentation
 For documentation head [here](hwtjac0.github.io/JAQ/) 
-## Tests
-Tests are written using QtTest. They are always built and run before pushing to repo (not yet). Rest...
-TODO
 ## Building
-TODO
+### Requirements
+Project was developed and built on linux distro. There is a chance that 
+it may build on windows without any issues.
+
+Required packages are:
+- Qt6 ^6.9
+  - Core
+  - Gui
+  - Quick
+  - Charts
+  - Widgets
+  - Location
+  - Positioning
+  - Test
+- cmake
+### Step-by-step
+Follow these commands:
+```
+git clone https://github.com/HWTjac0/JAQ.git
+cd JAQ
+mkdir build
+cd build
+cmake .. -DBUILD_TESTING=ON
+cmake --build .
+```
+## Tests
+Tests are written using QtTest and are run with Ctest. You can find them 
+in `Tests/` subdirectory.Basically after building the project and while still in `build/` 
+subdirectory, simply run `ctest` or `ctest -V`. If you want to only build
+tests instead of `cmake --build .` run `cmake --build . --target jaq_test`
--- Tests/test.cpp
+++ Tests/test.cpp
@@ -2,6 +2,8 @@
 #include <QObject>
 #include <QtTest/QtTest>
 #include "../Handlers/SensorDataHandler.h"
+#include "core/Database/databasereader.h"
+
 class TestAirQuality : public QObject {
     Q_OBJECT
 private slots:
@@ -9,18 +11,74 @@ private slots:
     void testParseIndicator();
     void testParseCity();
     void testParseSensors();
+    void testParseStation();
     void testCityToIndexEntry();
     void testStationToIndexEntry();
+private:
+    DatabaseReader testReader;
 };
 
 void TestAirQuality::initTestCase() {
     qDebug() << "Starting test suite...";
 }
-void TestAirQuality::testParseIndicator() {}
-void TestAirQuality::testParseCity() {}
-void TestAirQuality::testParseSensors() {}
-void TestAirQuality::testCityToIndexEntry() {}
-void TestAirQuality::testStationToIndexEntry() {}
+void TestAirQuality::testParseIndicator() {
+    QJsonObject indiObj;
+    indiObj["name"] = "name";
+    indiObj["code"] = "code";
+    Indicator ind = testReader.parseIndicator(indiObj);
+
+    QVERIFY(ind.code == "name");
+    QVERIFY(ind.name == "code");
+}
+void TestAirQuality::testParseCity() {
+    QJsonObject cityObj;
+    cityObj["name"] = "name";
+    cityObj["voivodeship"] = "voivodeship";
+    cityObj["county"] = "county";
+    cityObj["commune"] = "commune";
+    int cityId = 15;
+    City* city = testReader.parseCity(cityObj, cityId);
+
+    QVERIFY(city->name() == "name");
+    QVERIFY(city->voivodeship() == "voivodeship");
+    QVERIFY(city->county() == "county");
+    QVERIFY(city->commune() == "commune");
+    QVERIFY(city->id() == cityId);
+    delete city;
+}
+void TestAirQuality::testParseSensors() {
+    QJsonArray sensorsArr;
+    QJsonObject sensObj;
+    sensObj["id"] = "25";
+    sensObj["indicator_id"] = "25";
+    sensorsArr.append(sensObj);
+    Sensor& sensor = testReader.parseSensors(sensorsArr).first();
+    QVERIFY(sensor.id() == 25);
+    QVERIFY(sensor.indicatorId() == 25);
+}
+void TestAirQuality::testParseStation() {
+    QJsonObject stationObj;
+    stationObj["address"] = "address";
+    stationObj["id"] = "25";
+    Station station = testReader.parseStation(stationObj);
+    QVERIFY(station.id() == 25);
+    QVERIFY(station.address() == "address");
+}
+void TestAirQuality::testCityToIndexEntry() {
+    City city("name", "voivodeship", "county", "commune", 25);
+    QJsonObject cityObj = city.toIndexEntry();
+    QVERIFY(cityObj["id"] == 25);
+    QVERIFY(cityObj["name"] == "name");
+    QVERIFY(cityObj["voivodeship"] == "voivodeship");
+    QVERIFY(cityObj["county"] == "county");
+    QVERIFY(cityObj["commune"] == "commune");
+}
+void TestAirQuality::testStationToIndexEntry() {
+    Station station(25, "address");
+    QJsonObject stationObj = station.toIndexEntry();
+    QVERIFY(stationObj["id"] == 25);
+    QVERIFY(stationObj["name"] == "name");
+}
 QTEST_MAIN(TestAirQuality)
 
 #include "test.moc"
